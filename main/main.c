87b7292e6ca3d8cfbe2c9c8e82f02212b41d3287
Kirill <inscrut@gf.tom.ru>
2024-03-29 16:09:16 +0700

    Add skelet for ESP32-WROOM-32 board;

--- main/main.c
+++ main/main.c
@@ -1,6 +1,59 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+#include "esp_log.h"
+
+#include "driver/gpio.h"
+
+#define ESP_INTR_FLAG_DEFAULT 0
+
+#define BOARD_LED 2 //blue led
+#define BOARD_BTN 0 //BOOT btn, EN - reset btn
+#define GPIO_OUTPUT_PINS (1ULL << BOARD_LED)
+#define GPIO_INPUT_PINS (1ULL << BOARD_BTN)
+
+
 
 void app_main(void)
 {
+    //zero-initialize the config structure.
+    gpio_config_t io_conf = {};
+
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PINS;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    //interrupt of rising edge
+    //io_conf.intr_type = GPIO_INTR_NEGEDGE;
+    //bit mask of the pins
+    io_conf.pin_bit_mask = GPIO_INPUT_PINS;
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    int cnt = 0;
+    while (1) {
+        printf("cnt: %d\n", cnt++);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    vTaskSuspend(NULL);
 
 }
